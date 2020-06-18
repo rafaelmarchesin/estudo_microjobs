d1b7a126e2d75de591be127de7e592163de6cf52
Rafael Marchesin <rmarchesin@gmail.com>
2020-06-18 08:39:00 -0300

    feat: Primeiro commit

---
+++ src/Entity/User.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity("email", message="Esse e-mail já está em uso.")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     * @Assert\NotBlank(message="O campo nome não pode ser vazio.")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank(message="O campo email não pode ser vazio.")
+     * @Assert\Email(message="Informe um e-mail válido")
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank(message="O campo senha não pode ser vazio.")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $token;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Gegmo\Timestampable(on="create")
+     */
+    private $data_cadastro;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @Gegml\Timestampable(on="update")
+     */
+    private $data_alteracao;
+
+    /**
+     * @ORM\Column(type="jason_array")
+    */
+    private $roles;
+
+    //Isso é feito para validar o usuário por e-mail
+    public function __construct()
+    {
+        $this->status = false;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getDataCadastro(): ?\DateTimeInterface
+    {
+        return $this->data_cadastro;
+    }
+
+    public function setDataCadastro(\DateTimeInterface $data_cadastro): self
+    {
+        $this->data_cadastro = $data_cadastro;
+
+        return $this;
+    }
+
+    public function getDataAlteracao(): ?\DateTimeInterface
+    {
+        return $this->data_alteracao;
+    }
+
+    public function setDataAlteracao(?\DateTimeInterface $data_alteracao): self
+    {
+        $this->data_alteracao = $data_alteracao;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of roles
+     */ 
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * Set the value of roles
+     *
+     * @return  self
+     */ 
+    public function setRoles($roles)
+    {
+        $this->roles[] = $roles;
+
+        return $this;
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function eraseCredentials()
+    {
+        return null;
+    }
+}
